61c5d45aa63061d11bc8af6c07703a25bb0351cb
Loïc Combis <combis.loic@gmail.com>
2018-06-12 19:48:57 +1000

    v0.0.0

---
+++ autoResize.js
@@ -0,0 +1,49 @@
+var app = angular.module('loiccombis.auto-resize-textarea', []);
+
+app.directive("auto-resize-textarea", ['$compile', function($compile){
+
+
+
+        var updateHeight = function(scope,element){
+
+            if(!originalHeight){
+                originalHeight = element.height();
+            }
+
+            var content = element.val();
+
+            var parsedContent = content.replace(/[\n\r]/g,"<br/>");
+
+            parsedContent += "<br/>";
+
+            var id = Math.random().toString(36).substr(2,9);
+
+            var div = $compile("<div id=" + id + " style='display:none;overflow-wrap:break-word;white-space:pre-wrap;min-height:" + originalHeight + "px;width:" + element.width() + "px;font-size:" + element.css("font-size")+ "'>" + parsedContent + "</div>")(scope);
+
+            var body = angular.element(document).find('body');
+            
+            body.append(div);
+
+            element.height(Math.max(div.height(), originalHeight));
+
+           	angular.element('#'+id).remove();
+        };
+
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs, controller, transcludeFn) {
+
+                element[0].style.overflow = "hidden";
+                element[0].style.fontSize = "13px";
+
+                element.bind('keyup', function(){
+                   updateHeight(scope,element);
+                });
+                element.bind('focus', function(){
+                   updateHeight(scope,element);
+                });
+
+            }
+        };
+    }]);
+
